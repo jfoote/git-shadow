b6012ccf1a2ee511ab5ed5bd64b45dfeaa4f087e
Jonathan Foote <jmfoote@loyola.edu>
2015-01-19 21:58:38 -0500

    attempted to hack in test support; commiting in case i destory something

--- tests/test_git_shadow.py
+++ tests/test_git_shadow.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 from unittest import TestCase
-import subprocess, os
+import subprocess, os, shutil
 
 def rm_r(path):
-    import os, shutil
     if os.path.isdir(path):
         shutil.rmtree(path)
     elif os.path.exists(path):
@@ -15,12 +14,14 @@ class Test_git_shadow(TestCase):
         import tempfile
         self.repo_dir = tempfile.mkdtemp()
         subprocess.check_call(["git", "init", self.repo_dir])
+        shutil.copyfile("git_shadow.py", "git-shadow")
 
     def tearDown(self):
+        os.remove("git-shadow")
         rm_r(self.repo_dir)
 
     def test_activate(self):
-        subprocess.call(["./git-shadow", "activate", self.repo_dir])
+        subprocess.call(["git shadow", "activate", self.repo_dir])
 
         # verify git repo was initialized
         self.assertTrue(os.path.exists(os.path.join(self.repo_dir, ".shadow", ".git")))
@@ -30,15 +31,15 @@ class Test_git_shadow(TestCase):
 
     def test_add_hooks(self):
         # verify hooks are installed in parent repository
-        subprocess.call(["./git-shadow", "add-hooks", self.repo_dir])
+        subprocess.call(["git shadow", "add-hooks", self.repo_dir])
         self.assertTrue(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "pre-commit")))
         self.assertTrue(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "post-commit")))
         self.assertTrue(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "pre-checkout")))
 
     def test_remove_hooks(self):
         # verify hooks are removed from parent repository
-        subprocess.call(["./git-shadow", "add-hooks", self.repo_dir])
-        subprocess.call(["./git-shadow", "remove-hooks", self.repo_dir])
+        subprocess.call(["git shadow", "add-hooks", self.repo_dir])
+        subprocess.call(["git shadow", "remove-hooks", self.repo_dir])
         self.assertFalse(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "pre-commit")))
         self.assertFalse(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "post-commit")))
         self.assertFalse(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "pre-checkout")))
@@ -47,8 +48,8 @@ class Test_git_shadow(TestCase):
         filetext = "foobaz"
         filepath = os.path.join(self.repo_dir, ".git", "hooks", "pre-commit")
         open(filepath, "wt").write(filetext)
-        subprocess.call(["./git-shadow", "add-hooks", self.repo_dir])
-        subprocess.call(["./git-shadow", "remove-hooks", self.repo_dir])
+        subprocess.call(["git shadow", "add-hooks", self.repo_dir])
+        subprocess.call(["git shadow", "remove-hooks", self.repo_dir])
         self.assertTrue(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "pre-commit")))
         self.assertEqual(filetext, open(filepath, "rt").read())
         self.assertFalse(os.path.exists(os.path.join(self.repo_dir, ".git", "hooks", "post-commit")))
